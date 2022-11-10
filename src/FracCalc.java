791c71d510a14ab0df626d450e3bf7f4b55e1ee0
Justin Lee <91635048+555960cuhsd@users.noreply.github.com>
2022-11-09 22:23:53 -0800

    Upload final FracCalc code

--- src/FracCalc.java
+++ src/FracCalc.java
@@ -5,16 +5,29 @@ public class FracCalc {
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
         Scanner in = new Scanner(System.in);
-        boolean quit = false;
-        while (!quit){
-            System.out.println("What is your fraction expression? (N/D + N/D)");
+        while (true){
+            System.out.println("What is your fraction expression? Enter \"quit\" to exit and enter \"help\" for formatting/input help.");
             String fraction = in.nextLine();
             if (fraction.equals("quit")){
-                quit = true;
                 break;
             }
+            if (fraction.equals("help")){
+                System.out.println("************************************************* FORMATTING HELP *************************************************");
+                System.out.println("Your input should be an expression of two terms:");
+                System.out.println("[Fraction1 {operator} Fraction2]");
+                System.out.println("The operator has to be one of the four: \"+\", \"-\", \"*\", and \"/\".");
+                System.out.println("");
+                System.out.println("The fractions can expressed as either whole numbers, mixed numbers, improper fractions, or proper fractions.");
+                System.out.println("Proper and improper fractions are entered as \"n/d\", where \"n\" is the numerator number and \"d\" is the denominator number.");
+                System.out.println("Mixed numbers are put in the format \"w_n/d\", where \"w\" is the whole number. Whole numbers can be entered as just \"w\".");
+                System.out.println("If a fraction is negative, the negative sign \"-\" can be put in front of either w, n, or d.");
+                System.out.println("");
+                System.out.println("EXAMPLE INPUT: \"-4_3/5 - -7/3\"");
+                System.out.println("********************************************************************************************************************");
+                System.out.println("");
+                continue;
+            }
             System.out.println(produceAnswer(fraction));
-
         }
     }
 
@@ -28,23 +41,23 @@ public class FracCalc {
     //      e.g. return ==> "1_1/4"
 
 
-
     public static String produceAnswer(String input)
     {
         // TODO: Implement this function to produce the solution to the input
         Scanner expression = new Scanner(input);
-        expression.useDelimiter(" "); // Add expression.java?
+        expression.useDelimiter(" ");
         if (!expression.hasNext()){
             return "ERROR: Input is in an invalid format.";
         }
 
-        // Initializing the fractions
+        // Initializing the first fraction
         String fraction1 = expression.next();
         Fraction frac1 = new Fraction(fraction1);
         if (!validateFraction(fraction1)){
             return frac1.returnError();
         }
 
+        // Initializing the operator
         if (!expression.hasNext()){return "ERROR: Input is in an invalid format.";} // ERROR CHECK
         String operator = expression.next();
         String validOperators = "+-/*";
@@ -52,6 +65,7 @@ public class FracCalc {
             return "ERROR: Invalid operator in input";
         }
 
+        // Initializing the second fraction
         if (!expression.hasNext()){return "ERROR: Input is in an invalid format.";} // ERROR CHECK
         String fraction2 = expression.next();
         Fraction frac2 = new Fraction(fraction2);
@@ -83,16 +97,5 @@ public class FracCalc {
             return false;
         }
         return true;
-    }
-
-    public String addFraction(String fraction){
-        /*
-        validateFraction(fraction);
-        String[] fracSplit = fraction.split("+");
-        String frac1 = fracSplit.split[0];
-        int num1 = Integer.parseInt(split[0]);
-        int den1 = Integer.parseInt(split[1]);
-         */
-        return "";
     }
 }
